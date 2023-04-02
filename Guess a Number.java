f03b78e974d04a8a162c0ee5bc364b1a23e463e9
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-04-02 14:10:05 +0600

    Update Guess a Number.java

--- Guess a Number.java
+++ Guess a Number.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
 
-public class Main
-{
+public class encrypt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tries = 0;
         System.out.println("Enter Limit: ");
         int limit = sc.nextInt();
-        System.out.printf("\nGuess Between 1 - %d:\n", limit);
+        System.out.printf("Guess Between 1 - %d:\n", limit);
         int cGuess = (int) ((Math.random() * limit) + 1);
         //System.out.println(cGuess);
-        //System.out.println("Guess a Number: ");
         int uGuess = sc.nextInt();
-        while(uGuess != cGuess) {
+        while (uGuess != cGuess) {
             tries++;
-            if(cGuess > uGuess) {
+            if (cGuess > uGuess) {
                 System.out.println("Number is Higher");
             } else {
                 System.out.println("Number is Lower");
             }
             uGuess = sc.nextInt();
         }
-        System.out.println("You took "+ ++tries + " tries");
+        if (limit >= 1000 && tries < 5) {
+            System.out.println("Great Job. Out of 1000 or more choices you guessed the correct one in less than 5 tries.");
+        }
+        System.out.printf("%d is Correct\nYou took %d tries",cGuess,tries+1);
     }
 }
